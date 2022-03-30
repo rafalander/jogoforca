bb10e7cf62c5c2448941ec4d11635ee71ea46dc2
rafalander <rafa.lander@hotmail.com>
2022-03-30 10:05:41 -0300

    Update: initializing game and creat loop While

---
+++ jogo_forca.py
@@ -0,0 +1,16 @@
+def jogar():
+    print("***************************")
+    print("Bem vindo ao jogo de forca")
+    print("***************************")
+
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+        print("jogando...")
+
+    print("Fim do jogo")
+
+if(__name__ == "__main__"):
+    jogar()
