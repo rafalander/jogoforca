253b9aa01d029439279bb6726d7b9fbb9ae92af5
Rafael Lander Santos <rafa.lander@hotmail.com>
2022-03-31 18:28:26 -0300

    Update jogo_forca.py
    Update: add functions chute.strip()  and chute.upper()

--- jogo_forca.py
+++ jogo_forca.py
@@ -7,10 +7,20 @@ def jogar():
     enforcou = False
     acertou = False
 
-    while(not enforcou and not acertou):
-        print("jogando...")
+    while (not acertou and not enforcou):
+
+        chute = input("Qual letra? ")
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if (chute.upper() == letra.upper()):
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+            index = index + 1
+
+        print("Jogando...")
 
     print("Fim do jogo")
-
+    
 if(__name__ == "__main__"):
     jogar()
